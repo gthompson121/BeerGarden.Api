5c76b3503c0bf660c884e363a15c1877a0487acf
Gordon Thompson <gthompson121@gmail.com>
2019-05-28 18:24:14 +0100

    Update server.js

--- server.js
+++ server.js
@@ -10,9 +10,12 @@ const config = require('./config/config.js');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(function (req, res, next) {
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
 
+    if ( typeof req.headers.origin !== 'undefined' && req.headers.origin )
+    {
+      //do stuff if query is defined and not null
+      res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+    }
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
