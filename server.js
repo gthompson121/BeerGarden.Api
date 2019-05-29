0a6e59bdd9cc798952e8b554a438bcebe2c6be5a
Gordon Thompson <Gordon.Thompson@cashfac.com>
2019-05-29 08:00:50 +0100

    Add Access-Control-Allow-Origin header for localhost

--- server.js
+++ server.js
@@ -10,11 +10,16 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(function (req, res, next) {
 
-    if ( typeof req.headers.origin !== 'undefined' && req.headers.origin )
+    if (typeof req.headers.origin !== 'undefined' && req.headers.origin)
     {
-      //do stuff if query is defined and not null
-      res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+        console.log(req.headers.origin);
+        res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
     }
+    else
+    {
+        res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+    }
+
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
